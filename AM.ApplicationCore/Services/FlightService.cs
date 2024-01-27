36fb0c31b68470811e1702fbf8a62bbba1c71206
Hamdi Taha <hamdi.taha@esprit.tn>
2024-01-27 19:35:04 +0100

    Services

--- AM.ApplicationCore/Services/FlightService.cs
+++ AM.ApplicationCore/Services/FlightService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,28 @@ using System.Threading.Tasks;
 
 namespace AM.ApplicationCore.Services
 {
-    internal class FlightService
+    public class FlightService : IFlightService
     {
+        private ICollection _source;
+
+        // Constructeur
+        public FlightService(ICollection source)
+        {
+            _source = source;
+        }
+
+        // Implémentation de l'interface
+        public void ShowFlights(string filterType, string filterValue)
+        {
+            foreach (var flight in _source.Cast<Flight>()) // Supposant que _source contient des objets de type Flight
+            {
+                // Ajoutez ici la logique pour filtrer et afficher les vols selon les paramètres
+                Console.WriteLine($"Flight to {flight.Destination}, Date: {flight.FlightDate}, ID: {flight.FlightId}");
+            }
+            if (filterType != "Destination" && filterType != "FlightDate" && filterType != "FlightId")
+            {
+                throw new ArgumentException("Unknown filter");
+            }
+        }
     }
 }
