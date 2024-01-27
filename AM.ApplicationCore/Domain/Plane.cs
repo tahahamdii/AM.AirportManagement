69b31b9308f4c089c1a2663763f9738070e913d5
Hamdi Taha <hamdi.taha@esprit.tn>
2024-01-27 18:38:03 +0100

    Entities Created

--- AM.ApplicationCore/Domain/Plane.cs
+++ AM.ApplicationCore/Domain/Plane.cs
@@ -8,5 +8,22 @@ namespace AM.ApplicationCore.Domain
 {
     internal class Plane
     {
+        public int Capacity { get; set; }
+
+        public PlaneType PlaneType { get; set; }
+
+        public DateTime ManufactureDate { get; set; }
+
+        public Plane()
+        {
+            
+        }
+
+        public Plane(PlaneType planeType, DateTime manufactureDate,int capacity)
+        {
+            PlaneType = planeType;
+            Capacity = capacity;
+            ManufactureDate = manufactureDate;
+        }
     }
 }
