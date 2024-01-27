70cd938e37537acd350b0bf1aa8f3a7b690ad69c
Hamdi Taha <hamdi.taha@esprit.tn>
2024-01-27 18:28:01 +0100

    Ajoutez des fichiers projet.

---
+++ AM.ApplicationCore/Domain/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Dans le fichier Flight.cs
+namespace AM.ApplicationCore.Domain
+{
+    public class Flight
+    {
+        // Propriétés de l'entité Flight
+        public string Destination { get; set; }
+        public DateTime FlightDate { get; set; }
+        public int FlightId { get; set; }
+        // Ajoutez d'autres propriétés si nécessaire
+    }
+}
---
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Dans le fichier Passenger.cs
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+        // Propriétés de l'entité Passenger
+        public string PassengerName { get; set; }
+        // Ajoutez d'autres propriétés si nécessaire
+    }
+}
+
