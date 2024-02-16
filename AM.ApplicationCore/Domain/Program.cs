1d214ed9a8de8faf99d4861408b4be42421daa5e
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-16 01:24:03 +0100

    RechercheParReference

--- AM.ApplicationCore/Domain/Program.cs
+++ AM.ApplicationCore/Domain/Program.cs
@@ -8,5 +8,75 @@ namespace AM.ApplicationCore.Domain
 {
     internal class Program
     {
+        static List<Article> stock = new List<Article>();
+
+        static void Main(string[] args)
+        {
+            bool continuer = true;
+            while (continuer)
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Rechercher un article par référence");
+                Console.WriteLine("2. Ajouter un article au stock");
+                Console.WriteLine("3. Supprimer un article par référence");
+                Console.WriteLine("4. Modifier un article par référence");
+                Console.WriteLine("5. Rechercher un article par nom");
+                Console.WriteLine("6. Rechercher un article par intervalle de prix de vente");
+                Console.WriteLine("7. Afficher tous les articles");
+                Console.WriteLine("8. Quitter");
+                Console.Write("Votre choix: ");
+                int choix;
+                if (int.TryParse(Console.ReadLine(), out choix))
+                {
+                    switch (choix)
+                    {
+                        case 1:
+                            RechercherParReference();
+                            break;
+                        case 2:
+                            AjouterArticle();
+                            break;
+                        case 3:
+                            SupprimerArticle();
+                            break;
+                        case 4:
+                            ModifierArticle();
+                            break;
+                        case 5:
+                            RechercherParNom();
+                            break;
+                        case 6:
+                            RechercherParPrix();
+                            break;
+                        case 7:
+                            AfficherArticles();
+                            break;
+                        case 8:
+                            continuer = false;
+                            break;
+                        default:
+                            Console.WriteLine("Choix invalide.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrée invalide.");
+                }
+            }
+        }
     }
-}
+        
+        static void RechercherParReference()
+    {
+        Console.Write("Entrer la référence de l'article: ");
+        int reference = int.Parse(Console.ReadLine());
+        Article article = stock.Find(article => article.reference == reference);
+        if (article != null)
+        {
+            Console.WriteLine(article):
+        }else { Console.WriteLine("article not found")}
+    }
+
+        }
+   
